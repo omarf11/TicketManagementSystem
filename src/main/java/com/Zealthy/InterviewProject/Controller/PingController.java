9722a44023d16225ce16b74f05ddd154d873a2cf
omarf11 <ofaheem11@gmail.com>
2024-05-09 18:17:24 -0400

    adding ping

---
+++ src/main/java/com/Zealthy/InterviewProject/Controller/PingController.java
@@ -0,0 +1,9 @@
+package com.Zealthy.InterviewProject.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+    
+    //ping server to test connection
+}
