44109ad2ceba94d252f3dc85308c5379c4b8c5fc
omarf11 <ofaheem11@gmail.com>
2024-05-09 18:16:05 -0400

    shaping the controller

---
+++ src/main/java/com/Zealthy/InterviewProject/Controller/SupportEngineer.java
@@ -0,0 +1,9 @@
+package com.Zealthy.InterviewProject.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SupportEngineer {
+
+    //get support engineer by employeeId
+}
--- src/main/java/com/Zealthy/InterviewProject/Controller/TicketController.java
+++ src/main/java/com/Zealthy/InterviewProject/Controller/TicketController.java
@@ -7,13 +7,11 @@ public class TicketController {
     
     //create ticket
     //assign ticket to engineer
+    //update ticket status
     //remove engineer from ticket
     //get all tickets
-    //get all tickets for user
-    //get all tickets for support engineer
+    //get all tickets by user ID
+    //get all tickets by support engineer ID
     //update ticket status
     //add response to ticket
-
-    
-
 }
